efac4b4bbe044fce914fbf04ad3015204fd8c23a
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-03-30 21:06:26 -0300

    Create the object Product

--- AppMyPurchase/Models/Product.cs
+++ AppMyPurchase/Models/Product.cs
@@ -1,10 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SQLite;
 
 namespace AppMyPurchase.Models
 {
     public class Product
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public double Amout { get; set; }
+        public double Price { get; set; }
     }
 }
