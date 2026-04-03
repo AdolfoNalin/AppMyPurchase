53d5547247687c612124e7032b69e9517f1d333b
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-04-03 20:52:11 -0300

    Criando a tela de atualização de produto

--- AppMyPurchase/Views/UpdateProduct.xaml.cs
+++ AppMyPurchase/Views/UpdateProduct.xaml.cs
@@ -1,9 +1,36 @@
+using AppMyPurchase.Models;
+
 namespace AppMyPurchase.Views;
 
 public partial class UpdateProduct : ContentPage
 {
-	public UpdateProduct()
-	{
-		InitializeComponent();
-	}
+    public UpdateProduct()
+    {
+        InitializeComponent();
+    }
+    #region ToolbarItem_Clicked_Update
+    private async void ToolbarItem_Clicked_Update(object sender, EventArgs e)
+    {
+        try
+        {
+            Product p = BindingContext as Product;
+            Product product = new Product()
+            {
+                Id = p.Id,
+                Description = txtDescription.Text,
+                Amount = Double.Parse(txtAmount.Text),
+                Price = Double.Parse(txtPrice.Text),
+            };
+
+            List<Product> products = await App.Database.Update(product: product);
+
+            Navigation.PopAsync();
+
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Erro", $"{ex.Message}, {ex.StackTrace}, {ex.HelpLink}", "Fechar");
+        }
+    }
+    #endregion
 }
