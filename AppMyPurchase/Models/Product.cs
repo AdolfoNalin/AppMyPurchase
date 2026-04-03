43a2f1df5c7b0769a4f4194c50cb509008bc60b6
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-04-03 20:22:17 -0300

    Transformando a classe product em record

--- AppMyPurchase/Models/Product.cs
+++ AppMyPurchase/Models/Product.cs
@@ -2,12 +2,13 @@
 
 namespace AppMyPurchase.Models
 {
-    public class Product
+    public record class Product
     {
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
         public string Description { get; set; }
         public double Amount { get; set; }
         public double Price { get; set; }
+        public double Total => Amount * Price;
     }
 }
